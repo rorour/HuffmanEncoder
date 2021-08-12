43be17983131c317b3ef523d9bccef923a4827df
rorour <ravenmarieorourke@gmail.com>
2021-08-11 21:40:16 -0400

    Initial Commit

---
+++ HuffmanCoder/HuffmanCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuffmanCoderApp.swift
+//  HuffmanCoder
+//
+//  Created by Raven O'Rourke on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HuffmanCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
